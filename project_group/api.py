a22f37e80ed01d1dc0525956334b25c07c58dc83
SeanYew1 <109197831+SeanYew1@users.noreply.github.com>
2022-08-02 11:13:46 +0800

    Update api.py

--- project_group/api.py
+++ project_group/api.py
@@ -4,18 +4,12 @@ url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_cu
 response = requests.get(url)
 #print(response)
 data = response.json()
-print(data)
+
 
 exchange_rate = data
 exchange_rate_list = []
 for value in exchange_rate:
     exchange_rate_list.append(exchange_rate[value]["5. Exchange Rate"])
 
-statement=(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 - SGD{exchange_rate_list[0]}")
+statement=(exchange_rate_list[0])
 print (statement)
-# file_path=Path.cwd()/"summary_report.txt"
-# file_path.touch()
-
-# with file_path.open(mode="w") as file:
-#     for value_1 in exchange_rate_list:
-#         file.write(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 - SGD{value_1}")
