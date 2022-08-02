d2f929cdf62f5eb096a51285594d7106d9cb57aa
leesiying08 <110439247+leesiying08@users.noreply.github.com>
2022-08-02 15:08:28 +0800

    Rename profit_loss.py to deficit_report.py

---
+++ project_group/deficit_report.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+import csv, copy
+file_path=Path.cwd()/"csv_reports"/"profit-and-loss-usd(1).csv"
--- project_group/profit_loss.py
+++
@@ -1,3 +0,0 @@
-from pathlib import Path
-import csv, copy
-file_path=Path.cwd()/"csv_reports"/"profit-and-loss-usd(1).csv"
