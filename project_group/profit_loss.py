a5d149d4f50e1ea55475762a2b1b56a8a93abe66
yuxinn05 <110439132+yuxinn05@users.noreply.github.com>
2022-08-03 21:20:41 +0800

    Update profit_loss.py

--- project_group/profit_loss.py
+++ project_group/profit_loss.py
@@ -1,15 +1,32 @@
 # import Path method from pathlib
 from pathlib import Path
 #import csv function
-import csv,re
+import csv
 file=Path.cwd()/'csv_report'/'profit & loss.csv'
+emptylist=[]
+emptylist1=[]
 #read and open csv file 
-
-with file.open(mode="r",encoding="UTF-8") as file2:
-          #using .read to read file
-          text = file2.read()
-          # findall() function scans the string from left to right and finds all the matches of the pattern in the string
-          add = re.findall(pattern ="Net Profit", string=text)
-
-print(add)
-
+with file.open('r', encoding = 'UTF-8', newline = '') as file1:
+    # instantiate a reader object
+    list= csv.reader(file1)
+    # use `next()` to skip the header.
+    next(list)
+    for stats in list:
+        stats1=stats[0]
+        stats2=stats[4]
+        emptylist.append(stats1)
+        emptylist1.append(stats2)
+    #number of variables in list
+    count=len(emptylist)
+    #using for loop in enumerate empty list, starting from index 0
+    for count, profit_and_loss in enumerate(emptylist1, start=0):
+        #find difference between each index within range
+        profit_and_loss=[int(emptylist1[data+1])-int(emptylist1[data]) for data in range(len(emptylist)-1)]
+        #using for loop find x in cash_on_hand
+        for x in profit_and_loss:
+            #create scenario for when x negative
+            if x<0:
+                #execute print when x is negative
+                print(f"[PROFIT DEFICIT] DAY: {emptylist[profit_and_loss.index(x)]} , AMOUNT: SGD{abs(x)}")
+        #to break loop
+        break
