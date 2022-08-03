f5e825a8964579c9ad0f1357ad548e25347738e0
SeanYew1 <s10239934@connect.np.edu.sg>
2022-08-03 20:21:01 +0800

    commit

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -5,11 +5,66 @@ import csv
 # instantiate an file path object to current working directory
 # extend to file name 'project_group'/'csv_reports'/'cash-on-hand-usd.csv'
 file_p=Path.cwd()/'project_group'/'csv_reports'/'cash-on-hand-usd.csv'
-#read and open csv file in file_p 
-inputfile=csv.reader(open(file_p,'r'))
-# use for loop to iterate over a range object.
-for row in inputfile:
-    #access index position 1
-    rows=row[1]
-    #execute print
-    print(rows)
+# 3. Open file using `with` and `open` keyword in 'read' mode.
+# Include one more parameter, newline="".
+with file_p.open('r', encoding = 'UTF-8', newline = '') as file1:
+    # instantiate a reader object
+    list= csv.reader(file1)
+    # use `next()` to skip the header.
+    next(list)
+    #empty list for appending
+    empty_list=[]
+    #empty list for appending
+    empty_list1=[]
+    #using for loop find line in list
+    for line in list:
+        #access index 0
+        lines=line[0]
+        #converting string into integer
+        lines4=int(lines)
+        #access index 1
+        lines2=line[1]
+        #append the value to the empty_list.
+        empty_list.append(lines2)
+        #append the value to the empty_list1.
+        empty_list1.append(lines4)
+    #number of variables in list
+    count=len(empty_list1)
+    #using for loop in enumerate empty list, starting from index 0
+    for count, cash_on_hand in enumerate(empty_list, start=0):
+        #find difference between each index within range
+        cash_on_hand=[int(empty_list[data+1])-int(empty_list[data]) for data in range(len(empty_list1)-1)]
+        #using for loop find x in cash_on_hand
+        for x in cash_on_hand:
+            #create scenario for when x negative
+            if x<0:
+                #execute print when x is negative
+                print(f"[CASH DEFICIT] DAY: {empty_list1[cash_on_hand.index(x)]} , AMOUNT: SGD{abs(x)}")
+        #to break loop
+        break
+
+
+            
+
+
+     
+        
+    #if item>0:
+        #print("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+    #else:
+        #print(f"[CASH DEFICIT] DAY: {orders2.index(item)} , AMOUNT: {item}")
+        
+
+
+    
+   
+    
+    
+    
+            
+        
+        
+
+
+
+        
