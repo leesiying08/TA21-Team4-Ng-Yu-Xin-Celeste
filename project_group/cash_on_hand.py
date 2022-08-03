af45f8ef53cdb13aa64ab713288faf5a53a3b3ad
SeanYew1 <s10239934@connect.np.edu.sg>
2022-08-03 13:34:27 +0800

    commit

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -1,9 +1,16 @@
+# import Path method from pathlib
 from pathlib import Path
+
 import csv
+# instantiate an file path object to current working directory
+# extend to file name 'project_group'/'csv_reports'/'cash-on-hand-usd.csv'
 file_p=Path.cwd()/'project_group'/'csv_reports'/'cash-on-hand-usd.csv'
 inputfile=csv.reader(open(file_p,'r'))
+# use for loop to iterate over a range object.
 for row in inputfile:
+    #access index position 1
     rows=row[1]
+    #execute print
     print(rows)
         
             
