34bc082c3f3867a0f3265fd5c7026fe709550519
leesiying08 <110439247+leesiying08@users.noreply.github.com>
2022-08-04 15:05:14 +0800

    Update profit_loss.py

--- project_group/profit_loss.py
+++ project_group/profit_loss.py
@@ -1,42 +1,72 @@
 # import Path method from pathlib
 from pathlib import Path
 #import csv function
-import csv
-file=Path.cwd()/'csv_reports'/'Profits and Loss.csv'
-emptylist=[]
-emptylist1=[]
-#read and open csv file 
-file_path_1= Path.cwd()/"summary_report.txt"
-    #create the file "summary_report.txt" in current working directory
-file_path_1.touch()
-print(file_path_1.exists())
-def profit_loss():
-    with file.open('r', encoding = 'UTF-8', newline = '') as file1:
-    # instantiate a reader object
+import csv,requests
+def profitloss_function():
+    # instantiate an file path object to current working directory
+    """
+    -This function finds whether there is a surplus or deficit of cash on hand and convert the cash on hand to SGD
+    -This function also finds the day where the deficit happened.
+    """
+    url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=RV2RECVPN6VF1SHS"
+    response = requests.get(url)
+    #retrieve data with .json from response 
+    data = response.json()
+    #Save data as exchange_rate
+    exchange_rate = data
+    #create empty list for appending
+    exchange_rate_list = []
+    #Use for loop to iterate over a range object.
+    for value in exchange_rate:
+    #appending exchange rate into empty list
+        exchange_rate_list.append(exchange_rate[value]["5. Exchange Rate"])
+    #access index position 0
+        forex=(exchange_rate_list[0])
+        forex_1=float(forex)
+    # extend to file name 'csv_reports'/'Cash on Hand.csv'
+    file_p=Path.cwd()/'csv_reports'/'Profit and Loss.csv'
+    # 3. Open file using `with` and `open` keyword in 'read' mode.
+    # Include one more parameter, newline="".
+    with file_p.open('r', encoding = 'ascii',errors='ignore',newline = '') as file1:
+        # instantiate a reader object
         list= csv.reader(file1)
-    # use `next()` to skip the header.
-    next(list)
-        for stats in list:
-            stats1=stats[0]
-            stats2=stats[4]
-            emptylist.append(stats1)
-            emptylist1.append(stats2)
-    #number of variables in list
-        count=len(emptylist)
-    #using for loop in enumerate empty list, starting from index 0
-        with file_path_1.open(mode="w") as file:
-            for count, profit_and_loss in enumerate(emptylist1, start=0):
-        #find difference between each index within range
-                profit_and_loss=[int(emptylist1[data+1])-int(emptylist1[data]) for data in range(len(emptylist)-1)]
-        #using for loop find x in cash_on_hand
-            for x in profit_and_loss:
-            #create scenario for when x negative
-                if x<0:
-                #execute print when x is negative
-                    file.write(f"[PROFIT DEFICIT] DAY: {emptylist[profit_and_loss.index(x)]} , AMOUNT: SGD{abs(x)}")
-                elif x>0:
-                file.write(f"[NET PROFIT SURPLUS]:NET PROFIT EACH DAY IS HIGHER THAN THE PERVIOUS DAY")
-        #to break loop
-                break
+        # use `next()` to skip the header.
+        next(list)
+        #empty list for appending
+        empty_list=[]
+        #empty list for appending
+        empty_list1=[]
+        #using for loop find line in list
+        for line in list:
+            #access index 0
+            lines=line[0]
+            #converting string into integer
+            lines4=int(lines)
+            #access index 4
+            lines2=line[4]
+            #append the value to the empty_list.
+            empty_list.append(lines2)
+            #append the value to the empty_list1.
+            empty_list1.append(lines4)
+         #create a path object with file name "summary_report.txt"
+    file_path_1= Path.cwd()/"summary_report.txt"
+    #create the file "summary_report.txt" in current working directory
+    file_path_1.touch()
+    #open "summary_report.txt" file in write mode 
+    with file_path_1.open(mode="w") as file:
+        #number of variables in list
+        count=len(empty_list1)
+        #using for loop in enumerate empty list, starting from index 0
+        for count, profit_loss in enumerate(empty_list, start=0):
+            #find difference between each index within range
+            profit_loss=[int(empty_list[data+1])-int(empty_list[data]) for data in range(len(empty_list1)-1)]
+            #using for loop find x in cash_on_hand
+        for x in profit_loss:
+                    #execute print when x is negative
+            if x<0:
+                file.write(f"[PROFIT DEFICIT] DAY: {empty_list1[profit_loss.index(x)]} , AMOUNT: SGD{round(abs(x*float(forex_1)),1)} \n")
+           
+                
+print(profitloss_function())
    
    
