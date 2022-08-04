e89062a9be0e75e6ad350689fae9cbdce73a26ad
SeanYew1 <109197831+SeanYew1@users.noreply.github.com>
2022-08-04 20:18:12 +0800

    Update main.py

--- project_group/main.py
+++ project_group/main.py
@@ -1,9 +1,7 @@
-import api,coh,overheads,profit_loss
-
+import api, cash_on_hand, overheads, profit_loss
 def main():
-    forex = api.api_function
-    overheads.overhead_function(forex)
-    coh.coh_function(forex)
+    forex=api.api_function()
+    overheads.overheads_function(forex)
+    cash_on_hand.coh_function(forex)
     profit_loss.profitloss_function(forex)
-
-print(main())`
+main()
